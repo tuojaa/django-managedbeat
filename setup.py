464806a0a22a42fba14711e26d5138db9940039d
tuomas@humanyze.com <Tuomas Jaanu>
2016-10-19 13:37:02 +0300

    Bump version number to 0.1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='django-managedbeat',
-      version='0.0.2',
+      version='0.1.0',
       description='Django admin command to run celerybeat more reliably on Amazon Elastic Beanstalk',
       author='Tuomas Jaanu',
       author_email='tuomas@jaa.nu',
